eb57cfca49d72f3f125e4726bb2cd79450474b6d
Akishai <akishais18@gmail.com>
2024-08-03 15:13:16 -0400

    scholarship complete

--- AITUTOR/frontend/src/Content.jsx
+++ AITUTOR/frontend/src/Content.jsx
@@ -7,14 +7,65 @@ import './css/content.css';
 const Content = () => {
     const [textInput, setTextInput] = useState('');
     const [responseData, setResponseData] = useState([]);
+    const [isLGBTQChecked, setIsLGBTQChecked] = useState(false);
+    const [isNeedChecked, setIsNeedChecked] = useState(false);
+    const [average, setAverage] = useState(0);
+    const [race, setRace] = useState(''); 
+    const [gender, setGender] = useState(''); 
+    const [income, setIncome] = useState(''); 
 
     const handleInputChange = (event) => {
         setTextInput(event.target.value);
     };
 
+    const handleLGBTQCheckboxChange = (event) => {
+        setIsLGBTQChecked(event.target.checked);
+    };
+
+    const handleNeedCheckboxChange = (event) => {
+        setIsNeedChecked(event.target.checked);
+    };
+
+    const handleAverageChange = (event) => {
+        setAverage(event.target.value);
+    };
+
+    const handleRaceChange = (event) => {
+        setRace(event.target.value);
+    };
+
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+
+    const handleIncomeChange = (event) => {
+        setIncome(event.target.value);
+    };
+
     const handleSubmit = async () => {
+        let updatedInput = textInput;
+        if (isLGBTQChecked) {
+            updatedInput += " I am part of the LGBTQ community.";
+        } else {
+            updatedInput += " I am not part of the LGBTQ community.";
+        }
+        if (isNeedChecked) {
+            updatedInput += " Financial Need: Yes";
+        } else {
+            updatedInput += " Financial Need: No";
+        }
+        updatedInput += ` Average: ${average}`;
+        if (race) {
+            updatedInput += ` Race: ${race}`; 
+        }
+        if (gender) {
+            updatedInput += ` Gender: ${gender}`; 
+        }
+        if (income) {
+            updatedInput += ` Family Income: ${income}`; 
+        }
         try {
-            const response = await axios.post('http://localhost:5000/sort-scholarships', { input: textInput });
+            const response = await axios.post('http://localhost:5000/sort-scholarships', { input: updatedInput });
             setResponseData(response.data);
             toast.success('Submission successful!');  // Show success toast
         } catch (error) {
@@ -37,18 +88,18 @@ const Content = () => {
                     onChange={handleInputChange}
                 ></textarea>
                 <div className="stuff">
-                    {/* Other input fields */}
                     <label htmlFor="LGBTQ+">LGBTQ+</label>
-                    <input type="checkbox" id="LGBTQ+" name="LGBTQ+" />
+                    <input type="checkbox" id="LGBTQ+" name="LGBTQ+" checked={isLGBTQChecked} onChange={handleLGBTQCheckboxChange} />
                     
                     <label htmlFor="financial">Financial Need</label>
-                    <input type="checkbox" id="financial" name="financial" />
+                    <input  type="checkbox" id="financial" name="financial" checked={isNeedChecked} onChange={handleNeedCheckboxChange}/>
                     
                     <label htmlFor="average">Average</label>
-                    <input type="number" id="average" name="average" max="100" className="average"/>
+                    <input type="number" id="average"  name="average" max="100" className="average" value={average} onChange={handleAverageChange}/>
                     
                     <label className="race" htmlFor="race">Race: </label>
-                    <select id="race">
+                    <select id="race" value={race} onChange={handleRaceChange}>
+                        <option value="">Select Race</option>
                         <option value="White">White</option>
                         <option value="Black">Black</option>
                         <option value="Asian">Asian</option>
@@ -57,18 +108,20 @@ const Content = () => {
                     </select>
                     
                     <label htmlFor="gender">Gender: </label>
-                    <select id="gender">
+                    <select id="gender" value={gender} onChange={handleGenderChange}>
+                        <option value="">Select Gender</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Other">Other</option>
                     </select>
                     
                     <label htmlFor="income">Family Income: </label>
-                    <select id="income">
+                    <select id="income" value={income} onChange={handleIncomeChange}>
+                        <option value="">Select Income Range</option>
                         <option value="<$30,000">Less than $30,000 </option>
                         <option value="$30,000 - $60,000">$30,000 - $60,000</option>
                         <option value="$60,000 - $90,000">$60,000 - $90,000</option>
-                        <option value="$90,000 - $120,000">90,000 - $120,000</option>
+                        <option value="$90,000 - $120,000">$90,000 - $120,000</option>
                         <option value="$120,000 - $150,000">$120,000 - $150,000</option>
                         <option value="$150,000 +">$150,000 +</option>
                     </select>
@@ -80,10 +133,12 @@ const Content = () => {
 
             {responseData.length > 0 && (
                 <div>
-                    <h2>Top Scholarships:</h2>
-                    <ul>
+                    <h2 className="scholarship-title">Top Scholarships:</h2>
+                    <ul className="scholarship-unorder-list">
                         {responseData.map((item, index) => (
-                            <li key={index}>{item.Name} - {item.Amount}</li>
+                            <li className="scholarship-list" key={index}>
+                                {item.Name} - {item.Amount}
+                            </li>
                         ))}
                     </ul>
                 </div>
